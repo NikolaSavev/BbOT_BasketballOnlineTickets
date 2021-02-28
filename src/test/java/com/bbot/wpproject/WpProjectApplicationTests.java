05e4b90a563c5389b3f630023929e5763aa99dc6
Nikola Savev <nikolasavev@gmail.com>
2021-02-28 18:45:55 +0100

    Test commit

---
+++ src/test/java/com/bbot/wpproject/WpProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bbot.wpproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WpProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
